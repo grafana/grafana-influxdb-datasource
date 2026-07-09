421e820d5b02d5c30885f4ab88e72751e3a96e53
Ryan Morris <ryan.morris@grafana.com>
2026-07-09 10:39:42 -0500

    Chore: Revert replacing `uuidv4` with `generateUUID`

--- src/fsql/datasource.flightsql.ts
+++ src/fsql/datasource.flightsql.ts
@@ -1,4 +1,6 @@
-import { type DataSourceInstanceSettings, type TimeRange, generateUUID } from '@grafana/data';
+import { v4 as uuidv4 } from 'uuid';
+
+import { type DataSourceInstanceSettings, type TimeRange } from '@grafana/data';
 import { CompletionItemKind, type LanguageDefinition, type TableIdentifier } from '@grafana/plugin-ui';
 import { type TemplateSrv, getTemplateSrv } from '@grafana/runtime';
 import { COMMON_FNS, type DB, type FuncParameter, type SQLQuery, SqlDatasource, formatSQL } from '@grafana/sql';
@@ -57,7 +59,7 @@ export class FlightSQLDatasource extends SqlDatasource {
     }
     const interpolatedTable = this.templateSrv.replace(query.table);
     const queryString = buildColumnQuery(interpolatedTable, query.dataset);
-    const frame = await this.runSql<string[]>(queryString, { refId: `fields-${generateUUID()}` });
+    const frame = await this.runSql<string[]>(queryString, { refId: `fields-${uuidv4()}` });
     const fields = frame.map((f) => ({
       name: f[0],
       text: f[0],
